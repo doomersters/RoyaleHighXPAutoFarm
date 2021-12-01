5c89f7d00e700137a89429be6ac8c8b8e178b53e
Jonathan Greene <95334672+3xpl01t3r@users.noreply.github.com>
2021-11-30 22:48:02 -0800

    Update MainPlaceTP.lua

--- MainPlaceTP.lua
+++ MainPlaceTP.lua
@@ -1,4 +1,3 @@
 repeat task.wait() until game:IsLoaded()
 local _ = game:GetService("services.Players").LocalPlayer.Character or game:GetService("services.Players").LocalPlayer.CharacterAdded:Wait()
-
 game:GetService("ReplicatedStorage").SceptorTeleport:FireServer("BeachHouse")
