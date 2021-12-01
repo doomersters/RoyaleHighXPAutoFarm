917bb5f8a08fff2e03c6f1d1960fc6331c5171f5
Jonathan Greene <95334672+3xpl01t3r@users.noreply.github.com>
2021-12-01 09:57:25 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,9 +5,9 @@ local scripts = {
     ["735030788"] = game:HttpGet("https://raw.githubusercontent.com/3xpl01t3r/RoyaleHighXPAutoFarm/main/PlaceIdScripts/TeleportMenu.lua"),
     ["4728136927"] = game:HttpGet("https://raw.githubusercontent.com/3xpl01t3r/RoyaleHighXPAutoFarm/main/PlaceIdScripts/Beach.lua"),
     ["1765700510"] = game:HttpGet("https://raw.githubusercontent.com/3xpl01t3r/RoyaleHighXPAutoFarm/main/PlaceIdScripts/School.lua")
-}; coroutine.create(coroutine.resume(loadstring(scripts[tostring(game.PlaceId)]))
+}; coroutine.create(coroutine.resume(loadstring(scripts[tostring(game.PlaceId)])))
     
 if game.PlaceId ~= 735030788 then 
-    coroutine.create(coroutine.resume(loadstring(game:HttpGet("https://raw.githubusercontent.com/3xpl01t3r/RoyaleHighXPAutoFarm/main/UtilityScripts/SolveCaptcha.lua")))
-    coroutine.create(coroutine.resume(loadstring(game:HttpGet("https://raw.githubusercontent.com/3xpl01t3r/RoyaleHighXPAutoFarm/main/UtilityScripts/AntiAfk.lua")))
+    coroutine.create(coroutine.resume(loadstring(game:HttpGet("https://raw.githubusercontent.com/3xpl01t3r/RoyaleHighXPAutoFarm/main/UtilityScripts/SolveCaptcha.lua"))))
+    coroutine.create(coroutine.resume(loadstring(game:HttpGet("https://raw.githubusercontent.com/3xpl01t3r/RoyaleHighXPAutoFarm/main/UtilityScripts/AntiAfk.lua"))))
 end
