4b88a3ed92472dff46d1d332bb8b22f104bbd465
Jonathan Greene <95334672+3xpl01t3r@users.noreply.github.com>
2021-11-30 22:59:58 -0800

    Update TeleportMenu.lua

--- PlaceIdScripts/TeleportMenu.lua
+++ PlaceIdScripts/TeleportMenu.lua
@@ -1,3 +1,1 @@
-repeat task.wait() until game:IsLoaded()
-local _ = game:GetService("services.Players").LocalPlayer.Character or game:GetService("services.Players").LocalPlayer.CharacterAdded:Wait()
 game:GetService("ReplicatedStorage").SceptorTeleport:FireServer("BeachHouse")
