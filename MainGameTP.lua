54f31c7446d1b289fba4e8015db1cb63bc60d908
Jonathan Greene <95334672+3xpl01t3r@users.noreply.github.com>
2021-11-30 22:46:52 -0800

    Add files via upload

---
+++ MainGameTP.lua
@@ -0,0 +1,4 @@
+repeat task.wait() until game:IsLoaded()
+local _ = game:GetService("services.Players").LocalPlayer.Character or game:GetService("services.Players").LocalPlayer.CharacterAdded:Wait()
+
+game:GetService("ReplicatedStorage").SceptorTeleport:FireServer("BeachHouse")
