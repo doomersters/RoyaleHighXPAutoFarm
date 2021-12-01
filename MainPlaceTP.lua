1588f4c91797960f67c20b2166e09123adab8e7b
Jonathan Greene <95334672+3xpl01t3r@users.noreply.github.com>
2021-11-30 22:55:14 -0800

    Delete MainPlaceTP.lua

--- MainPlaceTP.lua
+++
@@ -1,3 +0,0 @@
-repeat task.wait() until game:IsLoaded()
-local _ = game:GetService("Players").LocalPlayer.Character or game:GetService("Players").LocalPlayer.CharacterAdded:Wait()
-game:GetService("ReplicatedStorage").SceptorTeleport:FireServer("BeachHouse")
