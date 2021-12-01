130cf8e0785a7d4d65c2ef2fabdf8d61ea7dd35e
Jonathan Greene <95334672+3xpl01t3r@users.noreply.github.com>
2021-11-30 23:24:39 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,6 +6,7 @@ local scripts = {
     ["4728136927"] = game:HttpGet("https://raw.githubusercontent.com/3xpl01t3r/RoyaleHighXPAutoFarm/main/PlaceIdScripts/Beach.lua"),
     ["1765700510"] = game:HttpGet("https://raw.githubusercontent.com/3xpl01t3r/RoyaleHighXPAutoFarm/main/PlaceIdScripts/School.lua")
 }; coroutine.create(coroutine.resume(loadstring(scripts[tostring(game.PlaceId)]))
+    
 if game.PlaceId ~= 735030788 then 
     coroutine.create(coroutine.resume(loadstring(game:HttpGet("https://raw.githubusercontent.com/3xpl01t3r/RoyaleHighXPAutoFarm/main/SolveCaptcha.lua")))
     coroutine.create(coroutine.resume(loadstring(game:HttpGet("https://raw.githubusercontent.com/3xpl01t3r/RoyaleHighXPAutoFarm/main/AntiAfk.lua")))
