62fb3d5d18a0b4d62d0bcb28e4af2f7f8ddfb295
Jonathan Greene <95334672+3xpl01t3r@users.noreply.github.com>
2021-12-03 15:32:45 -0800

    Update AntiAfk.lua

--- UtilityScripts/AntiAfk.lua
+++ UtilityScripts/AntiAfk.lua
@@ -1,8 +1,4 @@
-local VirtualUser = game:GetService("VirtualUser")
 while true do
-    local random = math.random(1,500)
-    VirtualUser:Button2Down(Vector2.new(0,random), workspace.CurrentCamera.CFrame)
-    task.wait(.5)
-    VirtualUser:Button2Up(Vector2.new(0,random), workspace.CurrentCamera.CFrame)
-    task.wait(480)
+    for _,v in next, getconnections(game:GetService("Players").LocalPlayer.Idled)) do v:Disconnect() end
+    task.wait()
 end
